0476087863513b9b2d93816c2e4c53400dfe1e32
Dowon Oh <dhehdnjs03@gmail.com>
2024-04-10 01:47:56 +0900

    20페이지까지 파싱

--- src/pages/api/crawl.ts
+++ src/pages/api/crawl.ts
@@ -17,29 +17,33 @@ interface Error {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<CrawlData[] | Error>) {
-  const targetUrl = 'https://www.tojobcn.com/bbs/board.php?bo_table=blog_go&sca=%EC%84%9C%EC%9A%B8&page=1';
+  const targetUrl = 'https://www.tojobcn.com/bbs/board.php?bo_table=blog_go&sca=%EC%84%9C%EC%9A%B8';
   const regex = /(\d+)/;
   try {
-    console.log('api 호출됨')
-    const {data} = await axios.get(targetUrl);
-    const $ = cheerio.load(data);
+    // page 1~20까지 반복 후 데이터 합쳐서 반환
     const result: CrawlData[] = [];
 
-    $('div.list-item').each((index, element) => {
-      const totalText = $(element).find('.contents > span:nth-of-type(2)').text().trim();
-      const total = parseInt(totalText.match(regex)?.[0] || '0');
-      const currentText = $(element).find('.contents > span:nth-of-type(1)').text().trim();
-      const current = parseInt(currentText.match(regex)?.[0] || '0');
-      result.push({
-        title: $(element).find('h2 > span:nth-of-type(2)').text().trim(),
-        link: $(element).find('.img-item > a').attr('href') || '',
-        imageUrl: $(element).find('.img-item img').attr('src') || '',
-        deadline: $(element).find('.label-cap_front').text().trim(),
-        total: total,
-        current: current,
+    for (let i = 1; i <= 20; i++) {
+      const {data} = await axios.get(targetUrl + `&page=${i}`);
+      const $ = cheerio.load(data);
+      $('div.list-item').each((index, element) => {
+        const totalText = $(element).find('.contents > span:nth-of-type(2)').text().trim();
+        const total = parseInt(totalText.match(regex)?.[0] || '0');
+        const currentText = $(element).find('.contents > span:nth-of-type(1)').text().trim();
+        const current = parseInt(currentText.match(regex)?.[0] || '0');
+
+        const deadlineString = $(element).find('.label-cap_front').text().trim()
+        const deadline =
+        result.push({
+          title: $(element).find('h2 > span:nth-of-type(2)').text().trim(),
+          link: $(element).find('.img-item > a').attr('href') || '',
+          imageUrl: $(element).find('.img-item img').attr('src') || '',
+          deadline: $(element).find('.label-cap_front').text().trim(),
+          total: total,
+          current: current,
+        })
       })
-    })
-    console.log(result)
+    }
     res.status(200).json(result);
   } catch(error) {
     res.status(500).json({message: "크롤링 중 에러 발생", error: error instanceof Error ? error.message : null});
